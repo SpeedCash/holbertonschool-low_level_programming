88d73e2f043d37d3e5dfff7634d60283df0cd20e
Super User <tcravero@gmail.com>
2023-10-17 01:34:02 -0700

    prints all possible combinations of single-digit numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,24 +8,18 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-		for (i = 0; i < 9; i++)
-		{
-		for (j = i + 1; j < 10; j++)
-		{
-
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (i < 8 || j < 9)
+	for (i = 0; i < 10; i++)
 	{
-			putchar(',');
-			putchar(' ');
+	putchar(i + '0');
+	if (i < 9)
+	{
+	putchar(',');
+	putchar(' ');
 	}
 	}
-	}
-			putchar('\n');
+	putchar('\n');
 
 	return (0);
 }

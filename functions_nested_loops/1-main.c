f1ac153d0a3d9499aad583a94121e334f08b680c
Super User <tcravero@gmail.com>
2023-10-18 00:15:24 -0700

    print the alphabet

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,5 +1,12 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-int _putchar(char c);
-void print_alphabet_x10(void);
-#endif
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}

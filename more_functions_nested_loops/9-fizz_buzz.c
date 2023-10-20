3eeace8db5c5df4d057c88d124891721e3fbeda6
Super User <tcravero@gmail.com>
2023-10-20 05:48:09 -0700

    The “Fizz-Buzz test” is an interview question

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,23 @@
 int main(void)
 {
 	int i;
-		for (i = 1; i <= 100; i++)
-		{
-			if (i % 15 == 0)
-				printf("FizzBuzz ");
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
 
-	else if (i % 3 == 0)
-		printf("Fizz ");
+		else if (i % 3 == 0)
+			printf("Fizz");
 
-	else if (i % 5 == 0)
-		printf("Buzz ");
+		else if (i % 5 == 0)
+			printf("Buzz");
 
-	else
-		printf("%d ", i);
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
 	}
-		printf("\n");
-		return (0);
+	printf("\n");
+	return (0);
 }

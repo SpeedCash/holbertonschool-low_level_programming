df6fccab56e9deea7ab8ed86cf241191dcda8d09
Super User <tcravero@gmail.com>
2023-10-30 04:00:42 -0700

    Write a function that returns the length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string using recursion.
+ * @s: The string whose length to find.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}

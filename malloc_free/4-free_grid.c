2e235dc3a0f9f6968ef6de4ad665e2a301a38fcb
SpeedCash <tcravero@gmail.com>
2023-11-07 11:36:41 +0100

    feur

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,9 +13,11 @@
 
 void free_grid(int **grid, int height)
 {
-    if (grid != NULL && height > 0) {
-        for (int i = 0; i < height; i++) {
-            free(grid[i]);
+    if (grid != NULL && height > 0)
+    {
+        for (int i = 0; i < height; i++)
+        {
+          free(grid[i]);
         }
         free(grid);
     }

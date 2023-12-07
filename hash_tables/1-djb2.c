f44dc2c84709ef925e0612db760e9b246d3e2a70
SpeedCash <tcravero@gmail.com>
2023-12-07 12:11:24 +0000

    Write a hash function implementing the djb2 algorithm.

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -11,13 +11,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash = 5381;
-    int c;
+	unsigned long int hash = 5381;
+	int c;
 
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 
-    return hash;
+	return (hash);
 }
